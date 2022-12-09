8968348e892c0d2b5ae032e0d64fc6e18fb68a15
ianandpandey <ianandpandey99@gmail.com>
2022-12-09 13:39:00 +0530

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,5 +46,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugins-google-fonts`,
+      options: {
+        fonts: [`roboto mono`, `muli\:400,400i,700,700i`],
+        display: "swap",
+      },
+    },
   ],
 };
