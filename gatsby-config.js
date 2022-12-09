6d48bfd63766c2185fa842e0bac6f1633ae2a9cb
ianandpandey <ianandpandey99@gmail.com>
2022-12-09 13:30:30 +0530

    add gatsby sharp plugin and mdx plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,21 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `getsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
   ],
 };
