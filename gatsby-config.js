00673b908e51665a3ff2e40450390c5d4f75f8b7
ianandpandey <ianandpandey99@gmail.com>
2022-12-09 13:11:18 +0530

    added gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,27 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
+};
