663bcd7b7470c935fa8b29190d27348c7c238779
ianandpandey <ianandpandey99@gmail.com>
2022-12-09 14:24:55 +0530

    add posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugins-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [`roboto mono`, `muli\:400,400i,700,700i`],
         display: "swap",
